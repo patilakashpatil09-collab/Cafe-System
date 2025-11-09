8579109a4bf77aac127b9e302221cd8633138f73
Akash Patil <patilakashpatil09@gmail.com>
2025-11-09 18:44:51 +0530

    Initial commit with Flask app

--- app.py
+++ app.py
@@ -399,4 +399,4 @@ def api_orders():
 
 # ---------- Run ----------
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
